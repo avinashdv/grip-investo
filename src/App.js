dcd9e057caf163267c1b7cfc285f2e94dbcd93a4
Avinash DV <avinash.dv.264@gmail.com>
2022-04-23 05:00:33 +0530

    modified react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import "./App.css";
 import Home from "./components/Pages/Home";
 import Calculator from "./components/Pages/Calculator";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Pages/Dashboard";
 function App() {
   return (
-    // <Router>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/calculator" element={<Calculator />} />
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
-    // </Router>
   );
 }
 
